686d3e34cc9ce8c13fbb5292507fefd58e4002a9
Deva <83206188+Deva3005@users.noreply.github.com>
2025-08-05 21:10:20 +0530

    Update advpy_stringPattern2.py

--- advTest05/advpy_stringPattern2.py
+++ advTest05/advpy_stringPattern2.py
@@ -1,3 +1,19 @@
+'''
+Another HomeWork 2:
+
+Write a program to find if two strings are same.
+two string are considered same if both strings have same letters in same order, but from different starting point
+eg abcd is same as bcda (a is moved to the right)
+abcd is same as cdab 
+abcd is  not same as cdba
+
+123456 = 456123
+123456 not = 412356
+hint - 
+there are many simple answers. you can try with slice function
+'''
+
+
 # print(s2[s2.index(s1[0]):]+s2[:s2.index(s1[0])])
 
 def checkString(s1:str,s2:str)->bool:
