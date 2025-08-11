4220bbf93a4e404ceac721941b774bb462de9406
devaSilverWolf <deva@silverforest.com>
2025-08-11 17:48:38 +0530

    Added Modulo ops is in best Denomination HW

--- Class01+Regex/class01+regex+01.py
+++ Class01+Regex/class01+regex+01.py
@@ -2,6 +2,10 @@
 # AUG 10 :: SUNDAY :: 2025
 
 r'''
+reference: JAI BAHUBALI !!!
+https://www.w3schools.com/python/python_regex.asp
+https://regex101.com/
+https://regexone.com/references/python
 
 ^        :: Starts with
 $        :: Ends with
@@ -21,6 +25,16 @@ re Methods
 findall("pattern","text")
 match("pattern","text")
 search("pattern","text")
+
+'''
+
+'''
+Home work problems for regular expression:
+
+1. Validate a phone number format like 123-456-7890.
+2. Check if a password has at least one uppercase, one lowercase, and one digit.
+3. Extract all hashtags from a social media post.
+4. Find all occurrences of 'cat' or 'dog' in a text
 
 '''
 
